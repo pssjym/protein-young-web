588cbff37dd9bf17af701d63ddae2247faa773ee
Fukuda-akiya <hukuda.akiya.s3@dc.tohoku.ac.jp>
2026-04-02 18:27:21 +0900

    updated .mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://protein-young-web.com',
-  base: '/protein-young-web',
 });
