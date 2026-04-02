face06cf13d7ec8a45b9ed005d705e5ed443e5c0
Fukuda-akiya <hukuda.akiya.s3@dc.tohoku.ac.jp>
2026-04-02 17:04:19 +0900

    edit src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pssjym.github.io',
+  base: '/protein-young-web', // リポジトリ名を / から書き始めます
+});
