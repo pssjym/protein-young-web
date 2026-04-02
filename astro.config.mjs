05296b9442f3833b1d376b4ba23339699be0ce79
Fukuda-akiya <hukuda.akiya.s3@dc.tohoku.ac.jp>
2026-04-02 17:48:28 +0900

    fix: 公式ドキュメントに従った

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pssjym.github.io',
-  base: '/protein-young-web', // リポジトリ名を / から書き始めます
+  base: '/protein-young-web',
 });
